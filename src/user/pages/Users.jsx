55bc73fc63c8f1dc8eccaf3a91c084cffc98a860
Alegma <6178232+alegmal@users.noreply.github.com>
2024-12-27 11:17:58 +0200

    top bar navigation

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -6,9 +6,9 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Aleg Ma",
+      name: "Aleg Malinovsky",
       image:
-        "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50&w=60",
+        "https://media.licdn.com/dms/image/v2/D4D03AQGkj-yHn2RToA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1669668073060?e=1740614400&v=beta&t=YV5yNrQFw2r2wsj21IdK0HDMO_8LttSdh37bX5grRIU",
       places: 3,
     },
   ];
