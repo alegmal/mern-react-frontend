daa825220a8b4f338e8a9714a7dc43a9c4aa7ee8
Alegma <6178232+alegmal@users.noreply.github.com>
2024-12-30 00:37:31 +0200

    adding map

--- src/shared/components/UIElements/Map.jsx
+++ src/shared/components/UIElements/Map.jsx
@@ -1,14 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 
 import "./Map.css";
+
 const Map = (props) => {
   const mapRef = useRef();
-  const map = new window.google.maps.Map(mapRef.current, {
-    center: props.center,
-    zoom: props.zoom,
-  });
 
-  new window.google.maps.Marker({ position: props.center, map: map });
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+    });
+
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);
+
   return (
     <div
       ref={mapRef}
