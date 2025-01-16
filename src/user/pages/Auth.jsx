0dc29237be7c3160356d64d96cc886e9e2f8f323
Alegma <6178232+alegmal@users.noreply.github.com>
2025-01-16 11:59:42 +0200

    authentication and react context

--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -1,69 +1,102 @@
-//TODO: form that renders password and email with validation (password length + email)
-import React from "react";
+import React, { useState } from 'react';
 
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
+import Card from '../../shared/components/UIElements/Card';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 import {
-  VALIDATOR_MINLENGTH,
   VALIDATOR_EMAIL,
-} from "../../shared/utils/validators";
-import { useForm } from "../../shared/hooks/form-hook";
-import Card from "../../shared/components/UIElements/Card";
-import "./Auth.css"
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from '../../shared/utils/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './Auth.css';
 
 const Auth = () => {
-  const [formState, inputHandle] = useForm(
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
-        value: "",
-        isValid: false,
+        value: '',
+        isValid: false
       },
       password: {
-        value: "",
-        isValid: false,
-      },
+        value: '',
+        isValid: false
+      }
     },
     false
   );
 
-  const placeSubmitHandler = (event) => {
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false
+          }
+        },
+        false
+      );
+    }
+    setIsLoginMode(prevMode => !prevMode);
+  };
+
+  const authSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
   };
 
-  const switchModeHandler = (event) => {
-    event.preventDefault();
-    console.log("switching to sign up");
-  };
-
   return (
-    <Card className="authenticcation">
-      <form onSubmit={placeSubmitHandler}>
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a name."
+            onInput={inputHandler}
+          />
+        )}
         <Input
-          id="email"
           element="input"
+          id="email"
           type="email"
-          label="Email"
+          label="E-Mail"
           validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email address"
-          onInput={inputHandle}
+          errorText="Please enter a valid email address."
+          onInput={inputHandler}
         />
         <Input
-          id="password"
           element="input"
+          id="password"
           type="password"
           label="Password"
           validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Your password must be minimum 5 characters long"
-          onInput={inputHandle}
+          errorText="Please enter a valid password, at least 5 characters."
+          onInput={inputHandler}
         />
-
         <Button type="submit" disabled={!formState.isValid}>
-          Login
+          {isLoginMode ? 'LOGIN' : 'SIGNUP'}
         </Button>
       </form>
-      <Button type="submit" onSubmit={switchModeHandler}>
-        Sign Up
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
       </Button>
     </Card>
   );
